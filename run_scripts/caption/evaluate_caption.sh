c73c56041fe6b38e44180ca8184d5bd13d2bdfb3
Xiaochen Zheng <xzheng.ethz@gmail.com>
2023-04-09 23:59:10 +0200

    debug

--- run_scripts/caption/evaluate_caption.sh
+++ run_scripts/caption/evaluate_caption.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                       # node count
 #SBATCH --gres=gpu:1   # titan_rtx & geforce_rtx_3090 & tesla_v100 & geforce_rtx_2080_ti & rtx_a6000
 #SBATCH --gres=gpu:rtx1080ti:1               # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=24G               # total memory per node (4 GB per cpu-core is default)
+#SBATCH --mem-per-cpu=16G               # total memory per node (4 GB per cpu-core is default)
 #SBATCH --time=18:00:00                 # total run time limit (HH:MM:SS)
 
 # Exit on errors
